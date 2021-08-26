c2babb5bd9e902f11643159e08c622ecf926ea16
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2021-08-26 13:31:11 +0200

    m.Page added

--- controller/MainNavigation/Example2.controller.js
+++ controller/MainNavigation/Example2.controller.js
@@ -3,7 +3,32 @@ sap.ui.define([
 ], function (Controller) {
     "use strict";
 
-    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.Example2");
+    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.Example2", {
+        onInit: function () {
+            var sURL = "/";
+            var amodel = new sap.ui.model.odata.ODataModel(sURL, true);
+            var oModel = new sap.ui.model.json.JSONModel();
+            amodel.read("/Employees", {
+                success: function (oRetrievedResult) {
+                    oModel.setData(oRetrievedResult);
+                },
+                error: function (oError) { alert(`Error ${oError}`); }
+            });
+            this.getView().setModel(oModel);
+        },
+        onPress: function (oEvent) {
+            var oItem, oCtx, oAttr;
+            oItem = oEvent.getSource();
+            oCtx = oItem.getBindingContext();
+            oAttr = oCtx.getProperty("EmployeeID");
+            this.getRouter().navTo("employee", {
+                employeeId: oAttr
+            });
+        },
+        getRouter: function () {
+            return sap.ui.core.UIComponent.getRouterFor(this);
+        }
+    });
 
     return Controller;
 });
